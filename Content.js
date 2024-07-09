f97ac5f2a463b7519eb95ca7b2533607441fe703
Potassium <brainiacgamer444@gmail.com>
2024-07-10 00:27:04 +0545

    Create function to decode URL if it has encoding
    URL encoding in old.reddit.com is done when user has not confirmed if
    they are above 18 or not. The function decodes the URL.
    However, only two encodings have been decoded yet which are "%3F" and
    "%2F"

--- Content.js
+++ Content.js
@@ -13,5 +13,31 @@ window.onload = () => {
     console.log("no www in url");
     console.log(location);
   }
+  decodeURL(search);
+  //   document.cookie = "over18=1;secure;domain=.reddit.com";
+  //   location.reload();
+  //   console.log(document.cookie);
   console.log("finished execution");
 };
+
+function decodeURL(url) {
+  let string;
+  if (url.includes("?dest=")) {
+    string = url.slice(url.indexOf("=") + 1);
+    console.log(string);
+  }
+
+  while (string.includes("%3A") || string.includes("%2F")) {
+    if (string.includes("%3A")) {
+      string = string.replace("%3A", ":");
+    }
+
+    if (string.includes("%2F")) {
+      string = string.replace("%2F", "/");
+    }
+  }
+  console.log(string);
+}
+
+// location.reload();
+// console.log("after reloading");
