eb8f4dd214b9211d5a0af30aab4611b378f83851
Potassium <brainiacgamer444@gmail.com>
2024-07-10 22:00:07 +0545

    Verify after loading page directly if asked
    If the old.reddit.com asks for 18+ verification, verify directly wihtout
    having to click continue button. I guess this will save sometime when
    you don't have to click continue on every page.

--- Content.js
+++ Content.js
@@ -1,28 +1,37 @@
-window.onload = () => {
+const excludedRedditPathnames = ["media", "gallery"];
+
+window.onload = async () => {
   const pathname = location.pathname;
   const protocol = location.protocol;
   const host = location.host;
   const search = location.search;
+  let modifiedURL;
   console.log(location);
   console.log(search);
   if (host.includes("www")) {
-    const modifiedURL = `${protocol}//${host.replace("www", "old")}${pathname}${search}`;
-    console.log(modifiedURL);
-    location.replace(modifiedURL);
+    if (!location.pathname.includes("media")) {
+      modifiedURL = `${protocol}//${host.replace("www", "old")}${pathname}${search}`;
+      console.log(modifiedURL);
+      location.replace(modifiedURL);
+    }
   } else {
     console.log("no www in url");
-    console.log(location);
+    if (location.pathname.includes("/over18")) {
+      const buttons = document.querySelectorAll(".c-btn");
+      buttons.forEach((button) => {
+        if (button.attributes.value.value === "yes") {
+          const continueButton = button;
+          continueButton.click();
+        }
+      });
+    }
   }
-  decodeURL(search);
-  //   document.cookie = "over18=1;secure;domain=.reddit.com";
-  //   location.reload();
-  //   console.log(document.cookie);
   console.log("finished execution");
 };
 
 function decodeURL(url) {
   let string;
-  if (url.includes("?dest=")) {
+  if (url.includes("?dest=") || url.includes("media")) {
     string = url.slice(url.indexOf("=") + 1);
     console.log(string);
   }
@@ -37,6 +46,7 @@ function decodeURL(url) {
     }
   }
   console.log(string);
+  return string;
 }
 
 // location.reload();
