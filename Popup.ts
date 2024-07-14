2183328b699cb9f8f3c143e91c3272a4ee89645a
Potassium <brainiacgamer444@gmail.com>
2024-07-14 23:07:12 +0545

    Handle storing in chrome's storage
    Create different functions to perform different tasks.
    Optimise code

--- Popup.ts
+++ Popup.ts
@@ -1,16 +1,30 @@
 const input = document.querySelectorAll(".toggleButton");
 let changeURL: boolean = false;
 
-function checkOptionsStatus() {
+// This part is only used for debugging
+chrome.storage.local.get(["changeRedditURL"], (result) => {
+  // Access the value from the result object
+  const extensionStatus = result.changeRedditURL;
+  console.log(extensionStatus);
+
+  // Use the value in an if statement
+  if (extensionStatus) {
+    // Perform actions when the status is true
+    console.log("Extension is enabled.");
+  } else {
+    // Perform actions when the status is false or undefined
+    console.log("Extension is disabled.");
+  }
+});
+
+async function setToggleStatus() {
   const redditButton = document.getElementById("toggleReddit");
-  if (localStorage.getItem("changeRedditURL") === "true") {
+  if (await checkExtensionStatus("changeRedditURL")) {
     redditButton.classList.add("checked");
     console.log("on");
-    //return true
   } else {
     redditButton.classList.remove("checked");
     console.log("off");
-    //return false
   }
 }
 
@@ -22,27 +36,43 @@ function sendMessageToWebPage(changeURL: boolean) {
   });
 }
 
+async function handleChromeStorage(element: Element) {
+  if (element.id === "toggleReddit") {
+    const extensionStatus = await checkExtensionStatus("changeRedditURL");
+    console.log(extensionStatus);
+    if (extensionStatus === true) {
+      chrome.storage.local.set({ changeRedditURL: false });
+      console.log("set reddit url change to false");
+      changeURL = false;
+      element.classList.remove("checked");
+    } else {
+      chrome.storage.local.set({ changeRedditURL: true });
+      console.log("set reddit url change to true");
+      changeURL = true;
+      element.classList.add("checked");
+    }
+  }
+}
+
+async function checkExtensionStatus(valueToGet: string): Promise<boolean> {
+  try {
+    const result = await new Promise<Record<string, boolean>>((resolve) => {
+      chrome.storage.local.get([valueToGet], (items) => resolve(items));
+    });
+    return result[valueToGet] === true;
+  } catch (error) {
+    console.log("Error occured while getting extension status: ", error);
+    // return false;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   input.forEach((element) => {
     element.addEventListener("click", () => {
-      if (element.id === "toggleReddit") {
-        const changeRedditURLStatus: string | null = localStorage.getItem("changeRedditURL");
-        if (changeRedditURLStatus === null || changeRedditURLStatus === "false") {
-          localStorage.setItem("changeRedditURL", "true");
-          console.log("set reddit url change to true");
-          changeURL = true;
-          element.classList.add("checked");
-        } else {
-          localStorage.setItem("changeRedditURL", "false");
-          console.log("set reddit url change to false");
-          changeURL = false;
-          element.classList.remove("checked");
-        }
-      }
-      console.log(element);
-      sendMessageToWebPage(changeURL);
+      handleChromeStorage(element);
+      //   sendMessageToWebPage(changeURL);
     });
   });
 
-  checkOptionsStatus();
+  setToggleStatus();
 });
