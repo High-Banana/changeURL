cbb7c2eae775841772af1a496f3526b7476fec88
Potassium <brainiacgamer444@gmail.com>
2024-07-14 17:07:26 +0545

    Store values in localstorage
    Does not work properly yet

--- Content.js
+++ Content.js
@@ -2,9 +2,22 @@ const input = document.querySelectorAll(".toggleButton");
 let isChangeRedditURLTrue = false;
 input.forEach((element) => {
     element.addEventListener("click", () => {
-        element.classList.toggle("checked");
-        if (element.id === "toggleReddit")
+        // element.classList.toggle("checked");
+        if (element.id === "toggleReddit") {
             isChangeRedditURLTrue = !isChangeRedditURLTrue;
+            console.log(localStorage.getItem("changeRedditURL"));
+            const changeRedditURLStatus = localStorage.getItem("changeRedditURL");
+            if (changeRedditURLStatus === null || changeRedditURLStatus === "false") {
+                localStorage.setItem("changeRedditURL", "true");
+                console.log("set reddit url change to true");
+                element.classList.add("checked");
+            }
+            else {
+                localStorage.setItem("changeRedditURL", "false");
+                console.log("set reddit url change to false");
+                element.classList.remove("checked");
+            }
+        }
         console.log(element);
     });
 });
--- Content.ts
+++ Content.ts
@@ -10,8 +10,21 @@ let isChangeRedditURLTrue: boolean = false;
 
 input.forEach((element) => {
   element.addEventListener("click", () => {
-    element.classList.toggle("checked");
-    if (element.id === "toggleReddit") isChangeRedditURLTrue = !isChangeRedditURLTrue;
+    // element.classList.toggle("checked");
+    if (element.id === "toggleReddit") {
+      isChangeRedditURLTrue = !isChangeRedditURLTrue;
+      console.log(localStorage.getItem("changeRedditURL"));
+      const changeRedditURLStatus: string | null = localStorage.getItem("changeRedditURL");
+      if (changeRedditURLStatus === null || changeRedditURLStatus === "false") {
+        localStorage.setItem("changeRedditURL", "true");
+        console.log("set reddit url change to true");
+        element.classList.add("checked");
+      } else {
+        localStorage.setItem("changeRedditURL", "false");
+        console.log("set reddit url change to false");
+        element.classList.remove("checked");
+      }
+    }
     console.log(element);
   });
 });
