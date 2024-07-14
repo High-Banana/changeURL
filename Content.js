f0f3e41fd84c3a16266ffeb69909b04acec585a1
Potassium <brainiacgamer444@gmail.com>
2024-07-14 14:04:23 +0545

    Initialise typescript and data types in Content.ts
    Installed typescript and the Content.js file is the compiled code of
    typescript (Content.js).

--- Content.js
+++ Content.js
@@ -1,52 +1,90 @@
-const excludedRedditPathnames = ["media", "gallery"];
-
-window.onload = async () => {
-  const pathname = location.pathname;
-  const protocol = location.protocol;
-  const host = location.host;
-  const search = location.search;
-  let modifiedURL;
-  console.log(location);
-  console.log(search);
-  if (host.includes("www")) {
-    if (!pathname.includes("media") && !pathname.includes("gallery")) {
-      modifiedURL = `${protocol}//${host.replace("www", "old")}${pathname}${search}`;
-      console.log(modifiedURL);
-      location.replace(modifiedURL);
-    }
-  } else {
-    console.log("no www in url");
-    if (location.pathname.includes("/over18")) clickContinueButton();
-  }
-  console.log("finished execution");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+var excludedRedditPathnames = ["media", "gallery"];
+window.onload = function () { return __awaiter(_this, void 0, void 0, function () {
+    var pathname, protocol, host, search, modifiedURL;
+    return __generator(this, function (_a) {
+        pathname = location.pathname;
+        protocol = location.protocol;
+        host = location.host;
+        search = location.search;
+        console.log(location);
+        console.log(search);
+        if (host.includes("www")) {
+            if (!pathname.includes("media") && !pathname.includes("gallery")) {
+                modifiedURL = "".concat(protocol, "//").concat(host.replace("www", "old")).concat(pathname).concat(search);
+                console.log(modifiedURL);
+                location.replace(modifiedURL);
+            }
+        }
+        else {
+            console.log("no www in url");
+            if (location.pathname.includes("/over18"))
+                clickContinueButton();
+        }
+        console.log("finished execution");
+        return [2 /*return*/];
+    });
+}); };
 function decodeURL(url) {
-  let string;
-  if (url.includes("?dest=") || url.includes("media")) {
-    string = url.slice(url.indexOf("=") + 1);
+    var string;
+    if (url.includes("?dest=") || url.includes("media")) {
+        string = url.slice(url.indexOf("=") + 1);
+        console.log(string);
+    }
+    while (string.includes("%3A") || string.includes("%2F")) {
+        if (string.includes("%3A")) {
+            string = string.replace("%3A", ":");
+        }
+        if (string.includes("%2F")) {
+            string = string.replace("%2F", "/");
+        }
+    }
     console.log(string);
-  }
-
-  while (string.includes("%3A") || string.includes("%2F")) {
-    if (string.includes("%3A")) {
-      string = string.replace("%3A", ":");
-    }
-
-    if (string.includes("%2F")) {
-      string = string.replace("%2F", "/");
-    }
-  }
-  console.log(string);
-  return string;
+    return string;
 }
-
 function clickContinueButton() {
-  const buttons = document.querySelectorAll(".c-btn");
-  buttons.forEach((button) => {
-    if (button.attributes.value.value === "yes") {
-      const continueButton = button;
-      continueButton.click();
-    }
-  });
+    var buttons = document.querySelectorAll(".c-btn");
+    buttons.forEach(function (button) {
+        var valueAttribute = button.getAttribute("value");
+        if (valueAttribute === "yes") {
+            var continueButton = button;
+            continueButton.click();
+        }
+    });
 }
---
+++ Content.ts
@@ -0,0 +1,53 @@
+const excludedRedditPathnames = ["media", "gallery"];
+
+window.onload = async () => {
+  const pathname: string = location.pathname;
+  const protocol: string = location.protocol;
+  const host: string = location.host;
+  const search: string = location.search;
+  let modifiedURL: string;
+  console.log(location);
+  console.log(search);
+  if (host.includes("www")) {
+    if (!pathname.includes("media") && !pathname.includes("gallery")) {
+      modifiedURL = `${protocol}//${host.replace("www", "old")}${pathname}${search}`;
+      console.log(modifiedURL);
+      location.replace(modifiedURL);
+    }
+  } else {
+    console.log("no www in url");
+    if (location.pathname.includes("/over18")) clickContinueButton();
+  }
+  console.log("finished execution");
+};
+
+function decodeURL(url: string): string {
+  let string: string;
+  if (url.includes("?dest=") || url.includes("media")) {
+    string = url.slice(url.indexOf("=") + 1);
+    console.log(string);
+  }
+
+  while (string.includes("%3A") || string.includes("%2F")) {
+    if (string.includes("%3A")) {
+      string = string.replace("%3A", ":");
+    }
+
+    if (string.includes("%2F")) {
+      string = string.replace("%2F", "/");
+    }
+  }
+  console.log(string);
+  return string;
+}
+
+function clickContinueButton() {
+  const buttons = document.querySelectorAll(".c-btn");
+  buttons.forEach((button: Element) => {
+    const valueAttribute = button.getAttribute("value");
+    if (valueAttribute === "yes") {
+      const continueButton = button as HTMLButtonElement;
+      continueButton.click();
+    }
+  });
+}
